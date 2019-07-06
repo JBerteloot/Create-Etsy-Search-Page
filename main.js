c387113ed9c106cb26c763f6aedfd3df1fdc8e58
Julie Berteloot <julie.berteloot.fr@gmail.com>
2019-07-05 17:27:27 -0700

    half-way 5JUL-1727

---
+++ main.js
@@ -0,0 +1,22 @@
+function makeItem(item) {
+  return `
+  <div class="item">
+          <img src="${item.img}" />
+          <p class="title">${item.title}</p>
+          <p class="seller">${item.seller}</p>
+          <p class="price">$${item.price}</p>
+        </div>
+  `
+}
+
+let htmlStr = ''
+items.results.forEach(item => {
+  htmlStr += makeItem({
+    img: item.Images[0].url_170x135,
+    title: item.title,
+    seller: item.Shop.shop_name,
+    price: item.price,
+  })
+});
+
+document.querySelector("#stuff").innerHTML = htmlStr
